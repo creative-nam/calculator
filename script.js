edfe3a64ddc961d6614b523903dd5e4578c97305
creative-username <myc82570@gmail.com>
2022-08-16 22:06:46 +0200

    Add calculate functionality Basicaally, the  main functionality of the project is now complete: user can now input two values and and operation and get the result for that equation

--- script.js
+++ script.js
@@ -32,3 +32,64 @@ function operate(operator, num1, num2) {
             return divide(num1, num2)
     }      
 }
+
+let equationDisplay = document.querySelector('.equationDisplay')
+let resultDisplay = document.querySelector('.resultDisplay')
+
+let valueBtns = document.querySelectorAll('.valueBtn')
+let operationBtns = document.querySelectorAll('.operationBtn')
+
+valueBtns.forEach(valueBtn => {
+    valueBtn.addEventListener('click', addToDisplay)
+});
+
+operationBtns.forEach(operationBtn => {
+    operationBtn.addEventListener('click', addOperation)
+})
+
+equalsBtn = document.querySelector('#equalsBtn')
+equalsBtn.addEventListener('click', calculate)
+
+function addToDisplay(e) {
+    let resultDisplayValue = resultDisplay.textContent
+    let currentBtnValue = e.target.textContent
+
+    if (currentBtnValue === '.' && resultDisplayValue.includes('.')) return
+    
+    if (resultDisplay.textContent === '0' && !isNaN(Number(currentBtnValue))) {
+        resultDisplay.textContent = currentBtnValue
+    }
+
+    else {
+        resultDisplay.textContent += currentBtnValue
+    }
+}
+
+function addOperation(e) {
+    let resultDisplayValue = resultDisplay.textContent
+    let operationBtnValue = e.target.textContent
+
+    if (resultDisplayValue.includes('+-x/')) return
+
+    equationDisplay.textContent = `${resultDisplayValue} ${operationBtnValue}`
+
+    resultDisplay.textContent = 0
+}
+
+function calculate() {
+    let resultDisplayValue = resultDisplay.textContent
+    let equationDisplayValue = equationDisplay.textContent
+
+    let operation = equationDisplayValue.slice(-1)
+
+    if (operation === '=') return
+
+    let num1 = Number(equationDisplayValue.slice(0, -2))
+    let num2 = Number(resultDisplayValue)
+
+    if (!(num1 && num2)) return resultDisplayValue
+
+    equationDisplay.textContent += ` ${resultDisplayValue} =`
+
+    resultDisplay.textContent = operate(operation, num1, num2)
+}
